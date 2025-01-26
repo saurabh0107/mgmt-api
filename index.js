81eafa97461add5b26d53ae529dabf9c1be8a761
unknown <saurabh.0107singh@gmail.com>
2025-01-26 19:24:38 +0530

    add

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const jwtSecret = "bsbsfbrnsftentwnnwnwn";
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-   cors()
+   cors({
+      origin: '*', // Allows all origins
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+   }
+   )
 );
 
 mongoose.connect(process.env.MONGO_URL);
